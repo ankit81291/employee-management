f8461e433f7312522614b3078125c1bc7750213a
unknown <I053006@INLN50838759A.apj.global.corp.sap>
2015-01-14 22:18:37 +0530

    integrating from Prasanna

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -86,14 +86,12 @@ module.exports.homelist = function(req, res){
     function(err, response, body) {
       var i, data;
       data = body;
-	  if(!err && response){
-		  if (response.statusCode === 200 && data.length) {
-			for (i=0; i<data.length; i++) {
-			  data[i].distance = _formatDistance(data[i].distance);
-			}
-		  }
+      if (response.statusCode === 200 && data.length) {
+        for (i=0; i<data.length; i++) {
+          data[i].distance = _formatDistance(data[i].distance);
+        }
+      }
       renderHomepage(req, res, data);
-	}
     }
   );
 };
