1981ae837e2349ae3857d2e0aee1462f81f9c985
unknown <I053006@INLN50838759A.apj.global.corp.sap>
2015-01-21 16:16:09 +0530

    Fixing the error in route of worker

--- app_api/routes/workers.js
+++ app_api/routes/workers.js
@@ -2,5 +2,5 @@ var ctrl = require('../controllers/workers');
 
 module.exports = function(app){
   app.get('/api/workers', ctrl.getWorkers);  
-  app.post('/api/tasks/create', ctrl.createWorker);
+  app.post('/api/workers/create', ctrl.createWorker);
 };
